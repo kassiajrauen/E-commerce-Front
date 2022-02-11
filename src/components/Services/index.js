0e230977c3e294b21099edc8c30cc2f0badaba67
Damon <damondias@gmail.com>
2022-02-11 13:57:49 -0400

    fix: Correction of selection logic

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -13,25 +13,29 @@ function Services() {
     const {token} = useContext(TokenContext);
    // const {user} = useContext(UserContext);
    // const { setPrice, setImage, setId, setDescription } = useContext(ServiceContext)
-    const [selected, setSelected] = useState();
+   
+    const [selected, setSelected] = useState(false);
 
     const [services, setServices] = useState([{
         id: 1,
         image: "https://www.petz.com.br/blog/wp-content/uploads/2021/03/como-dar-banho-em-cachorro2.jpg",
         description: "Banho",
-        price: "39.99"
+        price: "39.99",
+        selected: false
     },
     {
         id: 2,
         image: "https://www.naturedogh.com.br/imagens/onde-fazer-tosa-higienica-gato.jpg",
         description: "Tosa",
-        price: "59.99"
+        price: "59.99",
+        selected: false
     },
     {
         id: 3,
         image: "https://www.petz.com.br/blog/wp-content/uploads/2019/05/transporte-de-animais-onibus-metro-2.jpg",
         description: "Transporte",
-        price: "99.99"
+        price: "99.99",
+        selected: false
     }]);
 
     const config = {
@@ -57,28 +61,34 @@ function Services() {
     }
    
     function dataService(e, service) {
-       // e.preventDefault();
-        console.log(service);
+        // e.preventDefault();       
         // setPrice(service.price);
         // setImage(service.image);
          //setId(service.id);
-        // setDescription(service.description);       
-        setSelected(service.id);
+        // setDescription(service.description);  
+        if (service.selected === false){
+            service.selected = true;
+        }else{
+            service.selected = false;
+        }
+        
+        setSelected(service.selected)
         
     }
 
     function openShoppingCart(e){
         e.preventDefault();
+        
         navigate("/shopping-cart");
     }
-
+    
     return(
         <StyledServices>
             <h1> <img src='../../assets/logo.png' alt="logo"/> Escolha os serviços para seu Pet </h1>
 
             <div className="services-list">
                 {services.map(service => (
-                    <div onClick={(event)=> dataService(event, service)} key={service.id} className={`service ${selected === service.id? "selected": ""}`}> 
+                    <div onClick={(event)=> dataService(event, service)} key={service.id} className={`service ${ service.selected? "selected": ""}`}> 
                         <img src={service.image} alt="logo"/>
                         <h2>{service.description}</h2>
                         <h3>R$ {service.price}</h3>
